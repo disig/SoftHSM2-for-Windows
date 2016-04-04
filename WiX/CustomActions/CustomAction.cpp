0b84580330b8c59231a7048bdf994d95221d963e
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2016-04-04 23:41:54 +0200

    Updated year in copyright

--- WiX/CustomActions/CustomAction.cpp
+++ WiX/CustomActions/CustomAction.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Disig a.s. <opensource@disig.sk>
+ * Copyright (c) 2015-2016, Disig a.s. <opensource@disig.sk>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
